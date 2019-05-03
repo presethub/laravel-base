10c9cdbba31caa35745bc32de4056f8d7ddc7c07
Aris Ripandi <ar.is@outlook.com>
2019-05-04 00:23:58 +0700

    Fix authentication views

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name')->index();
             $table->enum('gender', ['Male', 'Female']);
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
